f097ede72ea9a082accd0d474c3182cbbad1d869
MicTel <suletm@wp.pl>
2014-03-20 13:18:36 +0100

    with Database

---
+++ Smart-install/Smart-install/control.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_install
+{
+    public class control
+    {
+        public List<string> getTags()
+        {
+            DatabaseArchiveEntities database = new DatabaseArchiveEntities();
+            List<Tag> tags = database.Tags.ToList();
+            List<string> tagList = new List<string>(); 
+            foreach (Tag tag in tags)
+            {
+                tagList.Add(tag.Name.ToString());
+            }
+
+            return tagList;
+        }
+    }
+}
