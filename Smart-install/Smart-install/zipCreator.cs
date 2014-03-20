5e38611d1837273f227742828dd1321d87094317
Ksrq <patryk.kosiorek@wp.pl>
2014-03-20 14:44:54 +0100

    obsluga zip (dodanieArch, usuwanieArch, rozpakowanieArch, dodanieDoArch) c.d.n

---
+++ Smart-install/Smart-install/zipCreator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using System.IO.Compression.ZipArchive;
+
+
+namespace Smart_install
+{
+    class zipCreator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Nazwa archiwum</param>
+        /// <param name="destinyPath">Gdzie ma powstać</param>
+        /// <param name="filePath">nazwa zip</param>
+        public static void createArchive(string name, string destinyPath, string filePath)
+        {
+            ZipFile.CreateFromDirectory(filePath, destinyPath, CompressionLevel.NoCompression, false);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="toAdd">co dodajemy</param>
+        /// <param name="whereAdd">gdzie dodajemy</param>
+        /// <param name="name">nazwa pliku</param>
+        public static void addToArchive(List<string> toAdd, string whereAdd, List<string> name)
+        {
+            foreach (string newFile in toAdd)
+            {
+                using (ZipArchive archive = ZipFile.Open(whereAdd, ZipArchiveMode.Update))
+                {
+                    archive.CreateEntryFromFile(newFile, "name");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pathArchive">sciezka do archiwum</param>
+        public static void deleteArchive(string pathArchive)
+        {
+            File.Delete(pathArchive);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath">nazwa zip</param>
+        /// <param name="extractPath">nazwa po rozpakowaniu</param>
+        public static void unzipFileFromArchive(string filePath, string extractPath)
+        {
+            ZipFile.ExtractToDirectory(filePath, extractPath);
+        }
+    }
+}
