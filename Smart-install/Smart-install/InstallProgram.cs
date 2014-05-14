5f8fa7923d5c471073ccae7f6d900a1f6a44bb8b
Tweester <tweester.pl@gmail.com>
2014-05-14 18:33:12 +0200

    Dodanie instalacji

---
+++ Smart-install/Smart-install/InstallProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace MyProcessSample
+{
+    /// <summary>
+    /// Shell for the sample.
+    /// </summary>
+    class InstallProgram
+    {
+
+
+
+        void StartInstallPrograms()
+        {
+
+            List<String> programList = new List<String>();
+
+            programList.Add("notepad");
+            programList.Add("mspaint");
+            programList.Add("calc");
+
+            foreach (String CurrentSetup in programList)
+            {
+                Process.Start(CurrentSetup);
+                while (Process.GetProcessesByName(CurrentSetup).Length != 0)
+                {
+                    Console.WriteLine("Uruchomiony " + CurrentSetup);
+                    Thread.Sleep(1000);
+                }
+
+            }
+        }
+
+
+    }
+}
